d3d7241f2c9fd084082ee66316378bac076f98df
Kevin Dehner <Kdehner@gmail.com>
2017-02-19 22:10:39 -0700

    Wrote start of game

--- Main.py
+++ Main.py
@@ -1,3 +1,8 @@
 
 while True:
-    #Write game here
+    print 'You are the one and only hero!'
+    print 'Destined to save the great land of Rain!'
+    print 'Please grace us with your name hero!'
+    print
+    name = raw_input('Enter your name: ')
+    print 'Hello ', name, '!' ' Welcome to your destiny!'
