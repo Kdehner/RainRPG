b02b923c8426bcbbd3517b1359011c7683c787e8
Nathan Reginato <Nathan.reginato@gmail.com>
2017-02-20 23:19:33 -0700

    Cleaned up store file

--- Store.py
+++ Store.py
@@ -1,5 +1,5 @@
 class Item(object):
-    """__init__() functions as the class constructor"""
+    #__init__() functions as the class constructor
     def __init__(self, itemName=None, itemType=None, cost=None):
         self.itemName = itemName
         self.itemType = itemType
@@ -7,15 +7,21 @@ class Item(object):
 
 class storeClass:
 
+    # Set up length vars
     itemCellWidth = 10;
     typeCellWidth = 10;
     costCellWidth = 10;
 
+    # Create item list
     itemList = []
-    itemList.append(Item("Sword", "offence", 10000000000))
-    itemList.append(Item("Shieldaaaaaaaaa", "defence", 200))
-    itemList.append(Item("Potion", "consumableeeeeeeeeeee", 300))
+    itemList.append(Item("Sword", "offence", 100))
+    itemList.append(Item("Shield", "defence", 200))
+    itemList.append(Item("Potion", "consumable", 300))
+    itemList.append(Item("Lance", "offence", 100))
+    itemList.append(Item("Super Awesome Sharp Thing", "offence", 2000))
+    itemList.append(Item("The Unbreakable Wall", "offence", 300000))
 
+    # Calculate lengths
     for item in itemList:
         if len(item.itemName) > itemCellWidth:
             itemCellWidth = len(item.itemName)
@@ -27,25 +33,30 @@ class storeClass:
             costCellWidth = len(str(item.cost))
 
     def createStore(self):
-        totleWidth = self.itemCellWidth + self.typeCellWidth + self.costCellWidth + 8
 
-        welcome = "Welcome to the shop!"
-        welcomeMargin = ((totleWidth - len(welcome))/2)
+        totalWidth = self.itemCellWidth + self.typeCellWidth + self.costCellWidth + 8
 
-        print '+' + '-' * totleWidth + '+'
-        print '|' + ' ' * totleWidth + '|'
-        print '|' + ' ' *  welcomeMargin + welcome + ' ' *  welcomeMargin + '|'
-        print '|' + ' ' * totleWidth + '|'
-        print '+' + '-' * totleWidth + '+'
+        welcomeMessage = "Welcome to the Shop!"
+        welcomeMargin = ((totalWidth - len(welcomeMessage))/2)
+        oddRound = 0 if totalWidth % 2 == 0 else 1
 
+        # Print store welcome banner
+        print '+' + '-' * totalWidth + '+'
+        print '|' + ' ' * totalWidth + '|'
+        print '|' + (' ' *  welcomeMargin) + welcomeMessage + (' ' *  (welcomeMargin + oddRound)) + '|'
+        print '|' + ' ' * totalWidth + '|'
+        print '+' + '-' * totalWidth + '+'
+
+        # Print column headers
         tTitle = 'Title' + ' ' * (self.itemCellWidth - len('Title'))
         tType = 'Type' + ' ' * (self.typeCellWidth - len('Type'))
         tCost = 'Cost' + ' ' * (self.costCellWidth - len('Cost'))
 
         print '| ' + tTitle + ' | ' + tType + ' | ' + tCost + ' |'
 
-        print '+' + '-' * totleWidth + '+'
+        print '+' + '-' * totalWidth + '+'
 
+        # Print item list
         for item in self.itemList:
             iName = item.itemName + ' ' * (self.itemCellWidth - len(item.itemName))
             iType = item.itemType + ' ' * (self.typeCellWidth - len(item.itemType))
@@ -53,6 +64,7 @@ class storeClass:
 
             print '| ' + iName + ' | ' + iType + ' | ' + iCost + ' |'
 
-        print '+' + '-' * totleWidth + '+'
+        print '+' + '-' * totalWidth + '+'
         print
-        userChoice = raw_input('What do you want?: ')
+
+        userChoice = raw_input('Make a selection: ')
