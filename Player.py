4ecb268a0cc2c7b3df022b7ec2d3d0be9bc42ab0
Kevin Dehner <Kdehner@gmail.com>
2017-02-22 21:04:30 -0700

    added stuff to player class

--- Player.py
+++ Player.py
@@ -1,5 +1,15 @@
 class playerClass:
 
+    def __init__(self, itemName, itemSlot):
+        self.itemName = itemName
+        self.itemSlot = itemSlot
+
+
     health = 100
     gold = 10
+    attack = 1
+    defense = 1
     items = []
+
+    armor = [('', 'Head'), ('', 'Sholders'), ('', 'Chest'), ('', 'Hands') ('', 'Legs'), ('', 'Feet')]
+    weapon = [('', 'Main Hand'), ('', 'OffHand')]
