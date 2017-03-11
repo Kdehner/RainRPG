48bdd03fb4f6e401bf1c5c57c65f488d89838240
Kevin Dehner <kevindehner@Emmas-MacBook-Pro.local>
2017-03-10 20:10:42 -0700

    Updated user_input

--- Main.py
+++ Main.py
@@ -1,11 +1,9 @@
 # coding=UTF-8
 # Import files
 from Store import storeClass, Item
-from Input import UserInputClass
 from Player import PlayerClass
 import os
-
-user_input = UserInputClass().user_input
+import user_input
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
@@ -17,10 +15,6 @@ introtxt = open('Intro.txt', 'r')
 for line in introtxt:
     print line,
 introtxt.close()
-
-# Get user name
-name = user_input('Enter your name: ')
-print 'Hello {} welcome to your destiny!'.format(name)
 
 # Define globals
 game = True
@@ -41,41 +35,50 @@ choice2ResponseArray = ['You suck!!!',
                         'You\'re heading out of town']
 
 while game:
-    # clear the screen
-    cls()
-    # If there are no more choices
-    if choiceCounter >= len(choice1Array):
-        game = False
-        print 'Game over'
-        quit()
-
-    print 'Oh mighty hero, will you?'
-    print '1)', choice1Array[choiceCounter]
-    print '2)', choice2Array[choiceCounter]
-    print
-
-    # User should be able to choose between two choices
-    userChoice = user_input('What will you decide: ')
+    user_input.user_input()
 
 
-    if userChoice == 'q':
-        quit()
-    else:
-        # clear the screen
-        cls()
-        if userChoice == '1':
-            if choice1ResponseArray[choiceCounter] == '{store}':
-                new_store = storeClass()
-                new_store.createStore()
-                new_item = new_store.get_purchased_item()
-                p = PlayerClass()
-                p.log_items()
-                p.log_person()
 
-                userChoice = raw_input('What will you decide: ')
-            else:
-                print choice1ResponseArray[choiceCounter]
-                choiceCounter += 1
-        else:
-            print choice2ResponseArray[choiceCounter]
-            choiceCounter += 1
+
+
+
+
+
+    # # clear the screen
+    # cls()
+    # # If there are no more choices
+    # if choiceCounter >= len(choice1Array):
+    #     game = False
+    #     print 'Game over'
+    #     quit()
+    #
+    # print 'Oh mighty hero, will you?'
+    # print '1)', choice1Array[choiceCounter]
+    # print '2)', choice2Array[choiceCounter]
+    # print
+    #
+    # # User should be able to choose between two choices
+    # userChoice = user_input('What will you decide: ')
+    #
+    #
+    # if userChoice == 'q':
+    #     quit()
+    # else:
+    #     # clear the screen
+    #     cls()
+    #     if userChoice == '1':
+    #         if choice1ResponseArray[choiceCounter] == '{store}':
+    #             new_store = storeClass()
+    #             new_store.createStore()
+    #             new_item = new_store.get_purchased_item()
+    #             p = PlayerClass()
+    #             p.log_items()
+    #             p.log_person()
+    #
+    #             userChoice = raw_input('What will you decide: ')
+    #         else:
+    #             print choice1ResponseArray[choiceCounter]
+    #             choiceCounter += 1
+    #     else:
+    #         print choice2ResponseArray[choiceCounter]
+    #         choiceCounter += 1
